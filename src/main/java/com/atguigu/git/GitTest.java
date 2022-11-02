917444886b5b925cc526846979dda7708cb96892
dubingchen <2712180275@qq.com>
2022-11-02 09:17:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Auther: 星辰
+ * @Date: 2022/11/2 9:13
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
